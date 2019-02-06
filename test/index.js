61bda66af7bbef950b194826d1f499d5f7129fde
Richard Memory <richardfmemory@gmail.com>
2019-02-06 15:23:29 -0700

    Add initial webhook polling

--- test/index.js
+++ test/index.js
@@ -5,23 +5,23 @@ const zapier = require('zapier-platform-core');
 const App = require('../index');
 const appTester = zapier.createAppTester(App);
 
-describe('Paladin and Archer Zapier Example', () => {
+describe('Testing the Paladin and Archer Zapier Example', () => {
 
 
-  it('should get hello', (done) => {
-    const bundle = {};
+	it('should get hello', (done) => {
+		const bundle = {};
 
-    appTester(App.triggers.aarc.operation.perform, bundle)
-      .then(results => {
-        should(results.length).above(0);
+		appTester(App.triggers.aarc_status_polling.operation.perform, bundle)
+			.then(results => {
+				should(results.length).above(0);
 
-        const firstResult = results[0];
-        console.log('test result: ', firstResult)
-        should(results[0].message).eql('Success');
+				const firstResult = results[0];
+				console.log('test result: ', firstResult)
+				should(results[0].message).eql('Success');
 
-        done();
-      })
-      .catch(done);
-  });
+				done();
+			})
+			.catch(done);
+	});
 
 });
